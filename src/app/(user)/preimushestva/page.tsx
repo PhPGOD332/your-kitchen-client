0e49695d27bb144324597f9799fee4c809a63350
Timur <tima.gizatullin.01@mail.ru>
2024-07-24 21:28:51 +0500

    Добавил блок с видео+слайдер фото на страницы: отзывы, портфолио(кухни), статьи, мебель, преимущества. Исправил фон на странице со статьями на темный. Добавил виджет 2гис в подвал.

--- src/app/(user)/preimushestva/page.tsx
+++ src/app/(user)/preimushestva/page.tsx
@@ -9,6 +9,18 @@ import { SITE_NAME, pagesData } from "@/shared/constants";
 import Advantages from "@/widgets/Advantages/Advantages";
 import OurTeam from "@/widgets/OurTeam/OurTeam";
 import { Metadata } from "next";
+import previewVideoSlider from "@/data/images/video-preview1.jpg";
+import { PhotoSlider } from "@/widgets/PhotoSlider/PhotoSlider";
+import img1 from "@/data/images/contacts-slider/MAIN (1).webp";
+import img2 from "@/data/images/contacts-slider/MAIN (2).webp";
+import img3 from "@/data/images/contacts-slider/MAIN (3).webp";
+import img4 from "@/data/images/contacts-slider/MAIN (4).webp";
+import img5 from "@/data/images/contacts-slider/MAIN (5).webp";
+import img6 from "@/data/images/contacts-slider/MAIN (6).webp";
+import img7 from "@/data/images/contacts-slider/MAIN (7).webp";
+import img8 from "@/data/images/contacts-slider/MAIN (8).webp";
+import img9 from "@/data/images/contacts-slider/MAIN (9).webp";
+import img10 from "@/data/images/contacts-slider/MAIN (10).webp";
 
 export const metadata: Metadata = {
   metadataBase: new URL(pagesData.advantages.url),
@@ -28,6 +40,19 @@ export const metadata: Metadata = {
   },
 };
 
+const initialImagesSlider = [
+  img1.src,
+  img2.src,
+  img3.src,
+  img4.src,
+  img5.src,
+  img6.src,
+  img7.src,
+  img8.src,
+  img9.src,
+  img10.src,
+];
+
 export const revalidate = 60;
 
 const getAdvantagesInfo = async () => {
@@ -41,6 +66,16 @@ const page = async () => {
   return (
     <>
       <Advantages />
+      <PhotoSlider
+        title={"Наше производство и технологии"}
+        photos={initialImagesSlider}
+        onlyVideo={true}
+        previewVideo={previewVideoSlider.src}
+        withoutLowerText={true}
+        firstVideoBlock={true}
+        bgColor='#2d2928'
+        wide={true}
+      />
       <OurTeam team={workers} />
       <LeaveRequestBlock
         location="Страница преимуществ"
